e51f2e89314d1957228f2038de0530c6b61758c5
Walid Wazini <wazini25@gmail.com>
2023-12-20 20:06:50 +0800

    update header UI n structure

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import MobileNav from '../MobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+import { noHeaderFooterUrls } from '../../../constants'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
+  return (
+    <nav className={[
+      classes.header,
+      noHeaderFooterUrls.includes(pathname) && classes.hide
+    ].filter(Boolean).join(' ')} >
+      <Gutter className={classes.wrap} >
+        <Link href={'/'} >
+          <img width={170} height={170} src='/logo-white.svg' alt='logo' />
+        </Link>
+        <HeaderNav header={header} />
+        <MobileNav />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
