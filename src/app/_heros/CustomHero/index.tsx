db588c5426a836f6e26ccb306646014e97328067
Walid Wazini <wazini25@gmail.com>
2023-12-23 03:46:38 +0800

    setup for custom Hero

---
+++ src/app/_heros/CustomHero/index.tsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+
+import { Page } from '../../../payload/payload-types'
+import { Gutter } from '../../_components/Gutter'
+import { CMSLink } from '../../_components/Link'
+import { Media } from '../../_components/Media'
+import RichText from '../../_components/RichText'
+
+import classes from './index.module.scss'
+
+export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) => {
+  const mediaUrl =
+    media &&
+    typeof media !== 'string' &&
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/media/${media.filename}`
+
+  return (
+    <Gutter className={classes.hero}>
+      Custom Hero Here
+    </Gutter>
+  )
+}
